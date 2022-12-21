36caf1de1ae91aead890a13040887218908f24e8
byhnd <120541041+byhnd@users.noreply.github.com>
2022-12-21 14:41:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,6 @@ function toggleMenu() {
 //toggle slide content
 
 var pages = document.querySelectorAll(".absinthe article");
-
 var pageToggle = function () {
   this.classList.toggle("flipped");
 };
@@ -55,7 +54,7 @@ for (var i = 0; i < pages.length; i++) {
   pages[i].addEventListener("click", pageToggle, false);
 }
 
-//function for toggling all slides at once
+//function for toggling all slides at onc
 
 function toggleAll() {
   for (var i = 0; i < pages.length; i++) {
@@ -64,7 +63,6 @@ function toggleAll() {
 }
 
 //arrow key navigation
-
 var Key = {
   SPACE: 32,
   LEFT: 37,
@@ -73,7 +71,7 @@ var Key = {
   DOWN: 40
 };
 
-/**
+/*
  * old IE: attachEvent
  * Firefox, Chrome, or modern browsers: addEventListener
  */
@@ -82,7 +80,7 @@ function _addEventListener(evt, element, fn) {
     element.addEventListener(evt, fn, false);
   } else {
     element.attachEvent("on" + evt, fn);
-  }
+  
 }
 
 function handleKeyboardEvent(evt) {
@@ -92,7 +90,7 @@ function handleKeyboardEvent(evt) {
   var keycode = evt.keyCode || evt.which; // also for cross-browser compatible
 
   var info = document.getElementById("info");
-  switch (keycode) {
+  switch (keycode) 
     case Key.UP:
       nextPage();
       break;
@@ -101,7 +99,7 @@ function handleKeyboardEvent(evt) {
       break;
     case Key.DOWN:
       prevPage();
-      break;
+      break
     case Key.RIGHT:
       nextPage();
       break;
@@ -110,7 +108,7 @@ function handleKeyboardEvent(evt) {
       break;
     default:
       void 0;
-  }
+  
 }
 
 _addEventListener("keydown", document, handleKeyboardEvent);
@@ -119,7 +117,7 @@ _addEventListener("keydown", document, handleKeyboardEvent);
 
 slider.addEventListener("swiped-left", function (e) {
   nextPage();
-});
+})
 
 slider.addEventListener("swiped-right", function (e) {
   prevPage();
@@ -128,11 +126,9 @@ slider.addEventListener("swiped-right", function (e) {
 slider.addEventListener("swiped-up", function (e) {
   nav.classList.add("open");
 });
-
 slider.addEventListener("swiped-down", function (e) {
   nav.classList.remove("open");
 });
-
 
 
 
@@ -146,7 +142,6 @@ slider.addEventListener("swiped-down", function (e) {
  * swiped-events.js - v1.1.6
 
  * Pure JavaScript swipe events
-
  * https://github.com/john-doherty/swiped-events
 
  * @inspiration https://stackoverflow.com/questions/16348031/disable-scrolling-when-touch-moving-certain-element
@@ -155,6 +150,6 @@ slider.addEventListener("swiped-down", function (e) {
 
  * @license MIT
 
- */
+ *
 
 !function(t,e){"use strict";"function"!=typeof t.CustomEvent&&(t.CustomEvent=function(t,n){n=n||{bubbles:!1,cancelable:!1,detail:void 0};var a=e.createEvent("CustomEvent");return a.initCustomEvent(t,n.bubbles,n.cancelable,n.detail),a},t.CustomEvent.prototype=t.Event.prototype),e.addEventListener("touchstart",function(t){if("true"===t.target.getAttribute("data-swipe-ignore"))return;s=t.target,r=Date.now(),n=t.touches[0].clientX,a=t.touches[0].clientY,u=0,i=0},!1),e.addEventListener("touchmove",function(t){if(!n||!a)return;var e=t.touches[0].clientX,r=t.touches[0].clientY;u=n-e,i=a-r},!1),e.addEventListener("touchend",function(t){if(s!==t.target)return;var e=parseInt(l(s,"data-swipe-threshold","20"),10),o=parseInt(l(s,"data-swipe-timeout","500"),10),c=Date.now()-r,d="",p=t.changedTouches||t.touches||[];Math.abs(u)>Math.abs(i)?Math.abs(u)>e&&c<o&&(d=u>0?"swiped-left":"swiped-right"):Math.abs(i)>e&&c<o&&(d=i>0?"swiped-up":"swiped-down");if(""!==d){var b={dir:d.replace(/swiped-/,""),touchType:(p[0]||{}).touchType||"direct",xStart:parseInt(n,10),xEnd:parseInt((p[0]||{}).clientX||-1,10),yStart:parseInt(a,10),yEnd:parseInt((p[0]||{}).clientY||-1,10)};s.dispatchEvent(new CustomEvent("swiped",{bubbles:!0,cancelable:!0,detail:b})),s.dispatchEvent(new CustomEvent(d,{bubbles:!0,cancelable:!0,detail:b}))}n=null,a=null,r=null},!1);var n=null,a=null,u=null,i=null,r=null,s=null;function l(t,n,a){for(;t&&t!==e.documentElement;){var u=t.getAttribute(n);if(u)return u;t=t.parentNode}return a}}(window,document);
