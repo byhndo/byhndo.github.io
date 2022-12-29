0e56ea2df825032c55367b99ef11d27aaeb3e599
byhnd <120541041+byhnd@users.noreply.github.com>
2022-12-29 14:04:43 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ new Vue({
       bg: 'bio' };
 
   } });
+
+
+$(window).scroll(function(){
+  scroll();
+});
+
+scroll();
+
+function scroll(){
+  var top = $('html, body').scrollTop();
+  var el = $("#distort").find("feDisplacementMap");
+  
+  el.attr('scale', top);
+  $("h1").css('opacity', 1 - (top * 0.007))
+}
