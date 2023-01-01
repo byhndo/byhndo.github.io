6cd4af41770d97c804033d6d1fe2e5653989eab4
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-01 16:52:56 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,3 +44,26 @@ tl.from(logo, {
 });
 
 
+
+
+
+
+Splitting();
+
+/* ---------------------------------- */
+
+/* Click to re-run */
+
+document.body.innerHTML += '<div style="position: absolute; top: 0; left: 0; right: 0; padding: .5em; font-size: 12px; font-family: monospace; text-align: center; pointer-event: none; ">👆Click to re-run</div>';
+
+var lr = document.querySelector('[data-splitting]');
+
+window.addEventListener('click',()=>{
+
+  var newone = lr.cloneNode(true);
+
+  lr.parentNode.replaceChild(newone, lr);
+
+  lr = newone;
+
+});
