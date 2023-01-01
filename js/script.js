18254095ba7986c50876f2d624cd8d63c54b3173
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-01 11:06:23 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,43 +8,5 @@ new Vue({
 })
 
 
-$(window).scroll(function(){
-  scroll();
-});
-
-scroll();
-
-function scroll(){
-  var top = $('html, body').scrollTop();
-  var el = $("#distort").find("feDisplacementMap");
-  
-  el.attr('scale', top);
-  $("h1").css('opacity', 1 - (top * 0.007))
-}
-
-
-
-
-
-
-const logo = document.querySelectorAll("p.sub1 span");
-
-
-let tl = gsap.timeline();
-
-tl.from(logo, {
-
-  duration: 1.8,
-
-  opacity: 0,
-
-  // ease: "bounce",
-
-  right: "100%",
-
-  stagger: .5,
-
-});
-
 
 
