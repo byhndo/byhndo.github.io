890f144f695d1d60d2576c14c8c292bbe3a820ef
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-07 11:24:14 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,6 +28,18 @@ function scroll(){
 
 
 
+AOS.init({
+  duration: 1000
+});
+
+   // AOS.init({
+   //    offset: 200,
+   //    duration: 600,
+   //    easing: 'ease-in-sine',
+   //    delay: 100,
+   //  });
+
+
 
 
 
