9d7d5c9ca4551edf438faf1208b660ece12d18f2
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-09 11:59:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,29 +30,3 @@ function scroll(){
 
 
 
-const boxes = document.querySelectorAll(".item");
-
-window.addEventListener("scroll", checkBoxes);
-
-checkBoxes();
-
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
-
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
-
-    if (boxTop < triggerBottom) {
-      box.classList.add("show");
-    } else {
-      box.classList.remove("show");
-    }
-  });
-}
-
-
-
-
-
-
-
