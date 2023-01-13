3df02244cfde75d84448c57145067ed954deaa37
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-13 11:28:36 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
 
+let revealContainers = document.querySelectorAll(".item");
+
+revealContainers.forEach((container) => {
+  let image = container.querySelector("img");
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      toggleActions: "restart none none reset"
+    }
+  });
+
+  tl.set(container, { autoAlpha: 1 });
+  tl.from(container, 1.5, {
+    xPercent: -100,
+    ease: Power2.out
+  });
+  tl.from(image, 1.5, {
+    xPercent: 100,
+    scale: 1.3,
+    delay: -1.5,
+    ease: Power2.out
+  });
+});
 
 
 
@@ -6,27 +30,3 @@
 
   
 
-gsap.registerPlugin(ScrollTrigger);
-
-let revealContainers = document.querySelectorAll(".item");
-
-revealContainers.forEach((container) => {
-  let image = container.querySelector("img");
-  let t1 = gsap.timeline({
-    scrollTrigger: {
-      trigger: container,
-    }
-  });
-  t1.set(container, { autoAlpha: 1 });
-  t1.from(container, 1.5, {
-    xPercent: -100,
-    ease: Power2.out
-  });
-
-  t1.from(image, 1.5, {
-    xPercent: 100,
-    scale: 1.3,
-    delay: -1.5,
-    ease: Power2.out
-  });
-})
