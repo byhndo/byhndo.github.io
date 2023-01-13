f3912c68d8a856fa6890bcb7f30e8861098f0c0d
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-13 11:03:25 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,28 +32,48 @@ function scroll(){
 
   
 
-gsap.registerPlugin(ScrollTrigger);
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.9
+};
 
-let revealContainers = document.querySelectorAll(".item");
+let revealCallback = (entries, self) => {
+  entries.forEach((entry) => {
+    let container = entry.target;
+    let img = entry.target.querySelector("img");
+    const easeInOut = "power3.out";
+    const revealAnim = gsap.timeline({ ease: easeInOut });
 
-revealContainers.forEach((container) => {
-  let img = container.querySelector("img");
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: container,
-      toggleActions: "restart none none reset"
+    if (entry.isIntersecting) {
+      revealAnim.set(container, {
+        visibility: "visible"
+      });
+      revealAnim.fromTo(
+        container,
+        {
+          clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+          webkitClipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)"
+        },
+        {
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+          webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+          duration: 1,
+          ease: easeInOut
+        }
+      );
+      revealAnim.from(img, 4, {
+        scale: 1.4,
+        ease: easeInOut,
+        delay: -1
+      });
+      self.unobserve(entry.target);
     }
   });
+};
 
-  tl.set(container, { autoAlpha: 1 });
-  tl.from(container, 1.5, {
-    xPercent: -500,
-    ease: Power2.out
-  });
-  tl.from(img, 1.5, {
-    xPercent: 100,
-    scale: 1.3,
-    delay: -1.5,
-    ease: Power2.out
-  });
+let revealObserver = new IntersectionObserver(revealCallback, options);
+
+document.querySelectorAll(".item").forEach((reveal) => {
+  revealObserver.observe(reveal);
 });
