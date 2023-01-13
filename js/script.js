a0ce0e1ebd0cd60180c486a74fb1cfccd21832b7
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-13 14:55:08 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      bg: 'bio'
+    }
+  }
+})
 
 
 
@@ -23,28 +30,3 @@ AOS.init({
 
 
 
-gsap.registerPlugin(ScrollTrigger);
-
-let revealContainers = document.querySelectorAll(".item");
-
-revealContainers.forEach((container) => {
-  let image = container.querySelector("img");
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: container,
-      toggleActions: "restart none none reset"
-    }
-  });
-
-  tl.set(container, { autoAlpha: 1 });
-  tl.from(container, 1.5, {
-    xPercent: -100,
-    ease: Power2.out
-  });
-  tl.from(image, 1.5, {
-    xPercent: 100,
-    scale: 1.3,
-    delay: -1.5,
-    ease: Power2.out
-  });
-});
