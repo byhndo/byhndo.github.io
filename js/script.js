ced141ea1611661a394e56471108b075649cf58c
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-13 14:15:42 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,36 @@ AOS.init({
   duration: 1000, 
   easing: 'ease-out-quad' 
 });
+
+
+
+
+
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let revealA = document.querySelectorAll(".item");
+
+revealA.forEach((a) => {
+  let image = a.querySelector("img");
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: a,
+      toggleActions: "restart none none reset"
+    }
+  });
+
+  tl.set(a, { autoAlpha: 1 });
+  tl.from(a, 1.5, {
+    xPercent: -100,
+    ease: Power2.out
+  });
+  tl.from(image, 1.5, {
+    xPercent: 100,
+    scale: 1.3,
+    delay: -1.5,
+    ease: Power2.out
+  });
+});
