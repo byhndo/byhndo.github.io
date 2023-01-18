4de99f024eb86968ea6b869205d4bfb8d11417fb
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-18 11:53:40 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ AOS.init({
 
 $(function() {
     var $window = $(window),
-        $body = $('body'),
-        $section = $('.bg');
+        $body = $('.bio'),
+        $section = $('.bio');
     $window.on("scroll", function() {
         var distance = $(this).scrollTop();
         $body.css('background-position', '0 -' + distance + 'px');
