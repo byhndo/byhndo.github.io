b69a39d065ba8a49949c152176365af8d075413b
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-18 17:01:34 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+new Vue({
+
+  el: '#app',
+
+  data() {
+
+    return {
+
+      bg: 'bio'
+
+    }
+
+  }
+
+})
+
+AOS.init();
+
+AOS.init({
+
+  offset: 120, 
+
+  delay: 0, 
+
+  duration: 1500, 
+
+  easing: 'ease-out-quad' 
+
+});
