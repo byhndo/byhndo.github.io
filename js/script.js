755b20435cff340d96246f85fbb34619fc39fb35
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-18 13:37:49 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,13 +18,3 @@ AOS.init({
 });
 
 
-(function() {
-    var bgLayer = document.getElementsByTagName('img');
-    window.onscroll = function() {
-        var top = pageYOffset;
-        document.body.style.backgroundPosition = '0 ' + (top*2) + 'px';
-        bgLayer[0].style.backgroundPosition = '0 ' + (top*0.5) + 'px';
-        bgLayer[1].style.backgroundPosition = '0 ' + (top*1.5) + 'px';
-        
-    };
-})();
