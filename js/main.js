b125a046b878ebaf86998fc5769c5ffb8778a085
byhnd <120541041+byhnd@users.noreply.github.com>
2023-01-25 18:25:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,40 @@ new Vue({
     return {
       bg: 'bio'
     }
-  }
+  },
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let revealContainers = document.querySelectorAll(".item");
+
+revealContainers.forEach((container) => {
+  let image = container.querySelector("img");
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container,
+      toggleActions: "restart none none reset"
+    }
+  });
+
+  tl.set(container, { autoAlpha: 1 });
+  tl.from(container, 1.5, {
+    xPercent: -100,
+    ease: Power2.out
+  });
+  tl.from(image, 1.5, {
+    xPercent: 100,
+    scale: 1.3,
+    delay: -1.5,
+    ease: Power2.out
+  });
+});
+
+
+
+
+
+
 })
 
 
