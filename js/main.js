909dfb8698e24a953d7d20566c0eb4681579c31d
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-05 12:42:23 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,13 +6,16 @@ const { createApp } = Vue
       }
     },
 mounted() {
-AOS.init();
-AOS.init({
-  easing: 'ease-out-sine',
-  once: false,
-  offset: 0
-});
-}
+    AOS.init({
+      // Global settings:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    })
+  },
   }).mount('#app')
 
 
