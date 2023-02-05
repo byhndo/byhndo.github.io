5b187f9e97a8e7d10935955fdb04086106a1a1b0
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-05 15:31:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,15 +52,6 @@ function setupReveal(container) {
 
 
 
-Splitting();
-
-
-$(document).ready(function() {
-$( ".dot" ).delay(100).animate({top:"0"},
-     3000, 'easeInOutBounce'
-
-)
-  });
 
 
 
