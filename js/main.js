29c522f14d1121e6bf9eedff246707bb69baca2b
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-07 12:13:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,14 +56,14 @@ setTimeout(() => {
   for (let i = 0; i < letters.length; i++) {
     anime({
       targets: letters[i],
-      easing: 'easeInOutSine',
+      easing: 'easeOutBack',
       translateX: ['5px', '0'],
       delay: 50 * i
     });
 
     anime({
       targets: letters[i],
-      easing: 'easeInOutSine',
+      easing: 'easeOutBack',
       opacity: 1,
       delay: 60 * i,
       complete: function(anim) {
@@ -78,7 +78,7 @@ setTimeout(() => {
 function showSubTitle() {
   anime({
     targets: '#sub-title',
-    easing: 'easeInOutSine',
+    easing: 'easeOutBack',
     opacity: 1,
     duration: 500,
     delay: 1
