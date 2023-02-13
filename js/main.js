928266bd8c4e3e5403b66830891bf572c7683873
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-13 20:18:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ textRevealElements.forEach((element) => {
         opacity: 0,
         top: "2rem",     
         stagger: .2,  
-        delay:.5,     
+        delay:1,     
         ease: "Elastic.easeOut"
       }); 
 
