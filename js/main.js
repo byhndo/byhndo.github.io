87ab5788da83ec6aee1b7dbda1e28bd1e04b8654
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-15 16:02:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ const text1 = box1.querySelectorAll(".text1, .text1 span");
     }); 
           
     tl.from(text1,  {      
-     duration: 2,
+     duration: 2.5,
      opacity: 0,
      top: "2rem",     
      stagger: .2,  
@@ -63,7 +63,7 @@ const text2 = box2.querySelectorAll(".text2, .text2 span");
           
     tl.from(text2,  {      
       opacity:0,
-      duration: 2,
+      duration: 2.5,
       delay: 1.3,
       top: "2rem",          
       ease: "Elastic.easeOut"
