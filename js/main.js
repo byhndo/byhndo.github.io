1a3690dc999db9f41535bcac3bec53b14a5f6753
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-17 09:39:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ const text2 = box2.querySelectorAll(".text2, .text2 span");
     }           
     }); 
           
-    tl.from(text2,  {           
+    tl.to(text2,  {           
       filter: "blur(0px)",
       duration: 2.5,
       delay: 1.3            
