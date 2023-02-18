710864e45b42c7c7bd901dee1e0682336545c700
byhnd <120541041+byhnd@users.noreply.github.com>
2023-02-18 16:39:57 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ setTimeout(() => {
       targets: letters[i],
       easing: 'easeInQuad',
       translateY: ['-2rem', '0'],
-      delay: 10 * i
+      delay: 0 * i
     });
 
     anime({
@@ -107,7 +107,7 @@ setTimeout(() => {
       easing: 'easeInQuad',
       opacity: 1,
       filter: "blur(0px)",
-      delay: 20 * i,
+      delay: 10 * i,
       complete: function(anim) {
         if (i === letters.length - 1) {
           showSubTitle()
