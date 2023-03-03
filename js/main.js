6000d72f5986bd2c2f4ea00450056552f0837a17
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-03 09:10:50 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,3 +115,10 @@ function setupReveal(container) {
 
     })
 }
+
+
+$(window).on('load', function() { 
+  $('#status').fadeOut(); 
+  $('#preloader').delay(350).fadeOut('slow'); 
+  $('body').delay(350).css({'overflow':'visible'});
+})
