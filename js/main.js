7d07057bd21a278fb55e9832269a5213b3138632
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-03 12:38:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,5 +120,5 @@ function setupReveal(container) {
 $(window).on('load', function() { 
   $('#status').fadeOut(); 
   $('#preloader').delay(1000).fadeOut('slow'); 
-  $('body').delay(350).css({'overflow':'visible'});
+  $('body').delay(1000).css({'overflow':'visible'});
 })
