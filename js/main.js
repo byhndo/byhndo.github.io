c437fd8d5f6b5f76fe86d0201839e9c5820e0bd8
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-10 20:23:28 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ function setupReveal(container) {
             tl.from(image, {
               duration: 7,                   
               scale: 1.5,
-              delay: -1,
+              delay: -2,
               ease: "Expo.easeInOut"
             }, 0);
         });
