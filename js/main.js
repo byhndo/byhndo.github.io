622ef32a9e3b661480d30ed28bd9a4e96c1fac80
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-10 11:53:45 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -131,14 +131,14 @@ function setupReveal(container) {
             tl.from(el, {
                 duration: 3,
                 scale: [0,1],
-                ease: "Bounce.easeOut"
+                ease: "Power2.easeOut"
             });
 
             tl.from(image, {
                 duration: 3,      
                 filter: "blur(10px)",
                 scale: 1.5,
-                ease: "Bounce.easeOut"
+                ease: "Power2.easeOut"
             }, 0);
         });
 
