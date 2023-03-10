e6e43b153816b99e801c6a2580bc3f5c7596d2e2
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-10 12:33:23 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ createApp({
          });
 
         gsap.from(".sub-title", {
-            delay: 4,
+            delay: 3.7,
             duration: 3,
             opacity: 0,         
             bottom: "-3rem", 
