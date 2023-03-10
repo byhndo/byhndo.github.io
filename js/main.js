11058c154fb16dd80e09f7ebb4ec2fbe7cc71642
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-10 15:37:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ function setupReveal(container) {
                 width: "100%",
                 opacity: 1,
                 filter: "blur(0px)",
-                ease: "Elastic.easeIn"
+                ease: "Bounce.easeOut"
             }, "<");
                                                                                                                 
         });
