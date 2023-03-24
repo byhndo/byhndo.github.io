15566682d1c86b801d1120990a5f83416a9ff48c
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-24 11:56:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ document.addEventListener("mouseover", (e) => {
 
   gsap.to(turbulence, {
     duration: durationTime,
-    startAt: { attr: { baseFrequency: 0.08 } },
+    startAt: { attr: { baseFrequency: 0.05 } },
     attr: { baseFrequency: 0 }
   });
 });
