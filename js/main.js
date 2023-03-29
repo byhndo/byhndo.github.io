3e3d9ba05501dd7038474e278f6b5567d0d4826f
byhnd <120541041+byhnd@users.noreply.github.com>
2023-03-29 09:01:10 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -192,6 +192,17 @@ document.addEventListener("mouseout", (e) => {
 
 
 
+const fadeOut = () => {
+const loaderWrapper = document.querySelector('.wrapper');
+loaderWrapper.classList.add('fade')
+}
+
+window.addEventListener('load', fadeOut);
+
+
+
+
+
 
     
 
