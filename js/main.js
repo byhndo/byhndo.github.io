8226e87be6cae936525e0eedb635338654264c79
byhnd <120541041+byhnd@users.noreply.github.com>
2023-04-03 13:56:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -200,7 +200,7 @@ function scroll(){
   var top = $('html, body').scrollTop();
   var el = $("#distort").find("feDisplacementMap");  
   el.attr('scale', top);
-  $("h1").css('opacity', 1 - (top * 0.007))
+  $("header").css('opacity', 1 - (top * 0.007))
 
 }
 
