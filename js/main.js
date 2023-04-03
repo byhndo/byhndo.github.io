31399c3ee11c1de707487641e9dc4937b92600fb
byhnd <120541041+byhnd@users.noreply.github.com>
2023-04-03 13:31:21 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -191,6 +191,20 @@ document.addEventListener("mouseout", (e) => {
 });
 
 
+$(window).scroll(function(){
+  scroll();
+});
+
+scroll();
+function scroll(){
+  var top = $('html, body').scrollTop();
+  var el = $("#distort").find("feDisplacementMap");  
+  el.attr('scale', top);
+  $("h1").css('opacity', 1 - (top * 0.007))
+
+}
+
+
 
 
 
