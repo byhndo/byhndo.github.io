74225caf3017d6eee2d4b072a0a68b5c7387fdc6
byhnd <120541041+byhnd@users.noreply.github.com>
2023-04-04 15:57:23 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ createApp({
          });
 
         gsap.from(".sub-title", {
-            delay: 10,
+            delay: 7,
             duration: 1,
             opacity: 0,         
             y: "3rem", 
@@ -50,7 +50,7 @@ createApp({
         });
         
         gsap.to(".ball", {
-            top: 0, delay: 12, opacity: 1, duration: 3, ease: "Bounce.easeOut"
+            top: 0, delay: 10, opacity: 1, duration: 3, ease: "Bounce.easeOut"
         });
 },                          
     data() {
