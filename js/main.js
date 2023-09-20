1b57c159f98dbe2ee70b38f4d3adafb8fbcd45cd
byhnd <120541041+byhnd@users.noreply.github.com>
2023-09-20 22:08:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -146,8 +146,7 @@ function setupReveal(container) {
 
             tl.from(image, {
               duration: 3,                   
-              scale: 1.5, 
-              filter: "grayscale(1)", 
+              scale: 1.5,              
               filter: "blur(2px)",
               delay: .3,
               ease: "Quad.easeOut"
