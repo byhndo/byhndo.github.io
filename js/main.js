4b09424f8d54a85c8002c15bb8798766b2efd33a
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-04 18:58:56 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@ function initBt1() {
   
   bt.addEventListener('click', function(e) {
     TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 20, y: isFF ? e.offsetY : e.offsetY + 20, width: 0, height: 0 } });
-    TweenLite.to(turb, 3, { attr: { x: '-=400', y: '-=400', width: 600, height: 600 } });
+    TweenLite.to(turb, 3, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
     TweenLite.fromTo(dm, 2, { attr: { scale: 30 } }, { attr: { scale: 0 } });
   });
 }
