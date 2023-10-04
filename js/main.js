004f55d489d4c3e2ca4153dc1929b0c9801965dc
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-04 10:53:32 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@ function initBt1() {
   
   bt.addEventListener('click', function(e) {
     TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 10, y: isFF ? e.offsetY : e.offsetY + 10, width: 0, height: 0 } });
-    TweenLite.to(turb, 3, { attr: { x: '-=250', y: '-=250', width: 500, height: 500 } });
+    TweenLite.to(turb, 3, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
     TweenLite.fromTo(dm, 2, { attr: { scale: 30 } }, { attr: { scale: 0 } });
   });
 }
@@ -202,7 +202,7 @@ function initBt2() {
   
   bt.addEventListener('click', function(e) {
     TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 10, y: isFF ? e.offsetY : e.offsetY + 10, width: 0, height: 0 } });
-    TweenLite.to(turb, 3, { attr: { x: '-=250', y: '-=250', width: 500, height: 500 } });
+    TweenLite.to(turb, 3, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
     TweenLite.fromTo(dm, 2, { attr: { scale: 30 } }, { attr: { scale: 0 } });
   });
 }
