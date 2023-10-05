0e40a123ffb13de9f1beb42ad2775c7d2baddd54
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-05 12:02:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -170,9 +170,7 @@ function scroll(){
 
 
 
-function getRandom(min, max){
-  return Math.random() * (max - min) + min;
-}
+
 
 var isSafari = /constructor/i.test(window.HTMLElement);
 var isFF = !!navigator.userAgent.match(/firefox/i);
@@ -190,7 +188,7 @@ function initBt1() {
   bt.addEventListener('click', function(e) {
     TweenLite.set(turb, { attr: { x: isFF ? e.offsetX : e.offsetX + 10, y: isFF ? e.offsetY : e.offsetY + 10, width: 0, height: 0 } });
     TweenLite.to(turb, 5, { attr: { x: '-=300', y: '-=300', width: 600, height: 600 } });
-    TweenLite.fromTo(dm, 3, { attr: { scale: 80 } }, { attr: { scale: 0 } });
+    TweenLite.fromTo(dm, 3, { attr: { scale: 30 } }, { attr: { scale: 0 } });
   });
 }
 
