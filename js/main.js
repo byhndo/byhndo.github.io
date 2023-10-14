3bf8c01bdac1ae3634962ee4327a65570336df88
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-14 23:24:59 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,7 +147,15 @@ tl.to(two, {
 	ease: "Quad.easeOut"
 });                                                  
 
-
+tl.set(ln, {			
+	width: "0%"
+});
+tl.to(ln, {
+	opacity: 1,
+	duration: 2,	
+	width: "100%",
+	ease: "Bounce.easeInOut"
+}, "<");
 	                                                                                                                  
 });
       
