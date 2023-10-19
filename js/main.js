650cc2982df9e72a9efb09ce5079511f990cccde
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-19 14:11:31 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ gsap.set(".ball", {
   transformOrigin: "center bottom"
 });	
 gsap.to(".ball", {
-  delay:7, 
+  delay:8, 
   duration:.7,
   top: 0,  
   scaleY: 2,
@@ -78,7 +78,7 @@ gsap.set(".sub-title span", {
 });
 gsap.to(".sub-title span", {
   opacity:1,
-  delay:6,
+  delay:4.5,
   duration:1,
   opacity: 1,	
   y: 0,
