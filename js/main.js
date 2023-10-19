c417087c65cbaf8a725b00e4247bf82eec282fcf
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-19 15:43:33 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ gsap.set(".sub-title span", {
 });
 gsap.to(".sub-title span", {
   delay:3,
-  duration: 3,
+  duration:1,
   y: 0,
   opacity: 1,	
   stagger: .1,
@@ -90,7 +90,7 @@ gsap.to(".sub-title span",  {
   rotate: 0,
   stagger: .1,			
   ease: "Elastic.easeOut",
-  duration:3
+  duration:2
 }, "-=1");	
 		
 },                          
