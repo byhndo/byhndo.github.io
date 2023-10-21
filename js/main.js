68d781aff92bddbb27c750a3a27154d1e3c1729e
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-21 09:13:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ data() {
 
 const tl = gsap.timeline({ paused: true });
 
-gsap.fromTo(".intro span", { opacity: 0 }, { opacity: 1, duration: 3, stagger:1, ease:Power3.easeOut });
+gsap.fromTo(".intro span", { opacity: 0 }, { opacity: 1, duration: 2, stagger:1, ease:Power3.easeOut });
 
 
 tl.to(".loader", {delay:3, opacity:0,duration:1,ease:Power3.easeOut
