2348551f385eb532fb854344a4ee1ff21abc14d3
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-25 15:48:51 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,7 +120,7 @@ function setupReveal(container) {
                         
 tl.set(one, {		
 	scaleY: 2, 	
-	y: "random(-10, 10)",
+	y: "random(-30, 30)",
 	rotate: "random(-180, 180)", 
 	transformOrigin: "50% 0%"
 });
