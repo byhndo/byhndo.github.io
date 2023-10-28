d66e71f7c3cfd8abdab79232404799029944fb08
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-28 20:18:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -135,13 +135,15 @@ tl.to(one, {
 	opacity: 1,
 	duration:1,
 	stagger: .1,
-	y:0,
 	ease: Back.easeOut.config(2.15)
 });
 tl.to(one, {
 	duration:1.5,
 	stagger: .1,
 	scaleY: 1,
+	x:0,
+	y:0,
+	rotate:0,
 	ease: "Quad.easeOut"
 }, "<+=0.2");
 
