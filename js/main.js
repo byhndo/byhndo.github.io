b9a0f80553a7aa1ff0dd20a3d0d56c86b09a2691
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-29 16:16:46 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -127,6 +127,7 @@ function setupReveal(container) {
 tl.set(one, {
 	scaleY: 2,
 	y:"random(-100, 100)",
+	z:"random(-100, 100)",
 	rotate:"random(-180, 180)",
 	transformOrigin: "0 50% -50"
 });
@@ -136,6 +137,7 @@ tl.to(one, {
 	duration:1,
 	stagger: .1,
 	y:0,
+	z:0,
 	ease: Back.easeOut.config(2.15)
 });
 tl.to(one, {
