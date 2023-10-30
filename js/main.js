b115c0a7a1cbdd7094c4f0afafabc7f74b545109
byhnd <120541041+byhnd@users.noreply.github.com>
2023-10-30 20:24:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -112,7 +112,7 @@ function setupReveal(container) {
         const RevealBoxs1 = container.querySelectorAll(".box1");
         RevealBoxs1.forEach((box1) => {
                 
-       const one = box1.querySelectorAll(".one, .one span, h3#liquid");
+       const one = box1.querySelectorAll(".one, .one span");
        let tl = gsap.timeline({
           scrollTrigger: {
             trigger: box1,
@@ -125,7 +125,7 @@ tl.to(one, {
 	opacity:1
 });
 		
-tl.to(one, {
+tl.to("h3#liquid", {
   attr: {
       scale: '0'
     },
