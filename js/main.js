35ef5a129ce3a194f5df3c5ce56b344144889abd
byhnd <120541041+byhnd@users.noreply.github.com>
2023-11-07 17:23:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,16 +17,16 @@ createApp({
     },
 mounted: function() {   
 gsap.fromTo("nav",{
-          clipPath: "polygon(0 0, 0 0, 0 0, 0 0)",
-          webkitClipPath: "polygon(0 0, 0 0, 0 0, 0 0)"
+clipPath: "polygon(0 0, 100% 0, 100%  0, 0 0)",
+webkitClipPath:"polygon(0 0, 100% 0, 100%  0, 0 0)"
         },
         {
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-          webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-	  delay:3.5,
-	  opacity:1,
-	  duration:3,
-          ease:Quad.easeOut
+clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",       
+	 delay:3,
+	 opacity:1,
+	 duration: 2,
+         ease:Quad.easeOut
         }
       );
 	
