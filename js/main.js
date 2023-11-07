bbdcadd40f2ed7feb552b9263fd8918c1cdc97d7
byhnd <120541041+byhnd@users.noreply.github.com>
2023-11-07 16:52:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ gsap.fromTo("nav",{
         {
           clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
           webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-	  delay:4.5,
+	  delay:3.5,
 	  opacity:1,
 	  duration:3,
           ease:Quad.easeOut
