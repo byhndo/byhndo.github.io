827fb5d2225872db546c64897f236fd5ebc0dfad
byhnd <120541041+byhnd@users.noreply.github.com>
2023-11-08 15:07:02 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -124,7 +124,7 @@ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
 webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",       
 	  opacity:1,
 	  duration: 3,
-    ease:Elastic.easeOut
+    ease:Bounce.easeOut
         }); 
 	   
    });
