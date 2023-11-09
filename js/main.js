3c8db5ed7d5bead7e30a3abd26f53ddcb3ca7547
byhnd <120541041+byhnd@users.noreply.github.com>
2023-11-09 08:01:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ data() {
 
 
 function headerAnimation() {
-const tl = gsap.timeline({paused:true});
+const tl = gsap.timeline();
 tl.set("nav", {
  autoAlpha:0
 });
@@ -114,7 +114,7 @@ function setupReveal(container) {
 const RevealSc = container.querySelectorAll(".sc");
    RevealSc.forEach((sc) => {                
     const pl = sc.querySelectorAll(".pl");
-    let tl = gsap.timeline({paused:true,
+    let tl = gsap.timeline({
     scrollTrigger: {
      trigger:sc,
      toggleActions: "restart none none none"
@@ -135,7 +135,7 @@ tl.to(pl, {
  const RevealBoxs1 = container.querySelectorAll(".box1");
    RevealBoxs1.forEach((box1) => {                
     const one = box1.querySelectorAll(".one, .one span");
-    let tl = gsap.timeline({paused:true,
+    let tl = gsap.timeline({
     scrollTrigger: {
      trigger: box1,
      toggleActions: "restart none none none"
@@ -169,7 +169,7 @@ const RevealBoxs2 = container.querySelectorAll(".box2");
  RevealBoxs2.forEach((box2) => {
  const two = box2.querySelectorAll(".two, .two span");            
  const ln = box2.querySelectorAll(".ln");                  
- let tl = gsap.timeline({paused:true,
+ let tl = gsap.timeline({
  scrollTrigger: {
   trigger: box2,
   toggleActions: "restart none none none"
@@ -198,7 +198,7 @@ tl.to(ln, {
 let revealContainers = container.querySelectorAll(".item");
  revealContainers.forEach((el) => {
  let image = el.querySelector("img");
- let tl = gsap.timeline({paused:true,
+ let tl = gsap.timeline({
  scrollTrigger: {
   trigger: el,
   ease: "Expo.easeOut",
@@ -233,7 +233,7 @@ let revealContainers = container.querySelectorAll(".item");
 }
 
 
-let loadTl = gsap.timeline({paused:true});
+let loadTl = gsap.timeline({});
 window.addEventListener('DOMContentLoaded', () => {
  gsap.to(".loader", {
  delay:10,
