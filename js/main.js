79c3c7469540c7ab1a6a6dce8455213ffdfaa465
byhnd <120541041+byhnd@users.noreply.github.com>
2023-11-12 00:22:35 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ data() {
     }
 }).mount('#app')  
 
+window.onload = function() {
 gsap.set("nav", {
  autoAlpha:0
 });
@@ -101,6 +102,7 @@ gsap.to(".sub-title span", {
 	rotate:0
 }, "<+=0.2");
 
+}	
 
 function setupReveal(container) {	
  container.ctx = gsap.context(() => {	  
