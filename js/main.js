e5ec08247556dced8d8b57c861d7be8bbaae1dc0
byhnd <120541041+byhnd@users.noreply.github.com>
2024-01-19 13:13:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ gsap.to("h1 span", {
  perspective:500,
  scaleY:0,
  y:-500,
- rotateX:"random(-100, 100)",
+ rotateX:"random(-180, 180)",
  transformOrigin: "30% 50% -50"
 });
 gsap.to("h1 span", {
