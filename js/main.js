50cc59b6732e664b6fc71f0af0ca0ff142d9c837
byhnd <120541041+byhnd@users.noreply.github.com>
2024-01-24 12:43:04 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,23 +65,27 @@ gsap.to(".ball", {
   ease: "Elastic.easeOut"
 }, "<+=.2");
 
-gsap.set(".char", {	     
-	scaleY:0, 	
-	y: "random(-200, 200)",
-	rotateX:"90deg",
-	rotateZ:"45deg",
-	transformOrigin: "50% 0% 50%"
-});
-gsap.to(".char", {
-	opacity:1,
-	duration:1,
-	stagger:.03,
-	ease: "Back.easeOut",	
-	scaleY: 1,
-	y:0,
-	rotateX:0,
-	rotateZ:0
-});
+	     
+function fallingText() {
+  const target = document.querySelector("#target");
+  const results = Splitting({ target: target, by: "chars" });
+  gsap.set("#target", { perspective: 400 });
+  gsap.from(".char", {
+    duration: .8,
+    opacity: 0,    
+    x: gsap.utils.random(-100, 100, true),
+    y: gsap.utils.random(-50, -100, true),
+    z: gsap.utils.random(0, 200, true),
+    rotate: gsap.utils.random(-90,90, true),
+    stagger:{
+        amount:3
+    },
+    repeat:-1,
+    repeatDelay:2,
+    yoyo:true       
+  });
+}
+fallingText()
 	
 
 function setupReveal(container) {	
