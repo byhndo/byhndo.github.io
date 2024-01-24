aad118f0d315d4e80270f9c083d0746c7eb76662
byhnd <120541041+byhnd@users.noreply.github.com>
2024-01-24 13:09:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,28 +65,21 @@ gsap.to(".ball", {
   ease: "Elastic.easeOut"
 }, "<+=.2");
 
-	     
-function fallingText() {
-  const target = document.querySelector("#sub-title");
-  const results = Splitting({ target: target, by: "chars" });
-  gsap.set("#sub-title", { perspective: 400 });
-  gsap.from(".char", {
-    duration: .8,
-    opacity: 0,    
-    x: gsap.utils.random(-100, 100, true),
-    y: gsap.utils.random(-50, -100, true),
-    z: gsap.utils.random(0, 200, true),
-    rotate: gsap.utils.random(-90,90, true),
-    stagger:{
-        amount:3
-    },
-    repeat:-1,
-    repeatDelay:2,
-    yoyo:true       
-  });
-}
-fallingText()
-	
+gsap.set(".sub-title .char", {
+  perspective:400,                  
+  x: gsap.utils.random(-100, 100, true),
+  y: gsap.utils.random(-50, -100, true),
+  z: gsap.utils.random(0, 200, true),
+  rotate: gsap.utils.random(-90,90, true)                    
+});
+gsap.to(".sub-title .char", {
+  duration:1, 
+  opacity:1, 
+  stagger:.02, 
+  x:0,y:0,z:0, 
+  rotate:0,
+  ease:"Circ.easeOut"
+});	       
 
 function setupReveal(container) {	
  container.ctx = gsap.context(() => {	  
