f1d2e7487605d5c25a3006cb1844b5d3028c0027
byhnd <120541041+byhnd@users.noreply.github.com>
2024-01-27 09:25:54 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ gsap.to("nav", {
 });
 
 gsap.to("h1 span", {
- yPercent:"-100%",
+ y:-500,
  scaleY:8,
  scaleX:0.5
 });
@@ -41,7 +41,7 @@ gsap.to("h1 span", {
  duration:.8,
  opacity:1,
  stagger:.05,
- yPercent:0,
+ y:0,
  scaleY:1,
  scaleX:1,
  ease:"Quint.easeInOut"	
