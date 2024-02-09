da6ff7a060587962d4212b64be4c4b8e7365dac7
byhnd <120541041+byhnd@users.noreply.github.com>
2024-02-09 09:12:15 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ tl.to(one, {
 const RevealBoxs2 = container.querySelectorAll(".box2");
  RevealBoxs2.forEach((box2) => {
  const two = box2.querySelectorAll(".two, .two span");            
- const ln = box2.querySelectorAll(".line");                  
+ const ln = box2.querySelectorAll("hr");                  
  let tl = gsap.timeline({
  scrollTrigger: {
   trigger: box2,
