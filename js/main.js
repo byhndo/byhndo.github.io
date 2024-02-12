2e3f54cd85d8d0090639b374ada76d4441f7d4d6
byhnd <120541041+byhnd@users.noreply.github.com>
2024-02-12 15:49:50 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,19 @@ data() {
         }
     }
 }).mount('#app')  
-		
+
+gsap.set("nav", {
+ opacity:0,
+ y:-100
+});
+gsap.to("nav", {
+ delay:5,
+ duration:3,
+ opacity:1,
+ y:0,
+ ease:Circ.easeOut
+});
+	
 gsap.set("h1 span", {
  y:"-100%",
 });
