76c5ba7bc91b7103d2fb854c80785226c987399e
byhnd <120541041+byhnd@users.noreply.github.com>
2024-02-29 13:01:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -127,7 +127,12 @@ tl.fromTo(pl, {
 	 
  const RevealBoxs1 = container.querySelectorAll(".box1");
    RevealBoxs1.forEach((box1) => { 
-    const one = box1.querySelectorAll(".one, .one .char");
+	   
+  $('.one').each(function(){
+  $(this).html($(this).text().replace(/\S/g, "<span>$&</span>"));
+  });
+	   
+    const one = box1.querySelectorAll(".one span");
     let tl = gsap.timeline({
     scrollTrigger: {
      trigger: box1,
