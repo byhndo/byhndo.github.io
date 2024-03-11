8e7b2e04ed83d8ba85d09682dd57db1a63b5b9ba
byhnd <120541041+byhnd@users.noreply.github.com>
2024-03-11 11:36:03 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,25 +50,7 @@ data() {
             bg: 'bio'
         }
     }
-}).mount('#app')  
-
-
-
-
-var change_meta = document.getElementById("wtr2");
-var meta_color = document.createElement("meta");
-    meta_color.setAttribute("name","theme-color");
-    meta_color.setAttribute("content","#323235");
-    document.getElementsByTagName("head")[0].appendChild(meta_color);
-function change_color(){  
-  if(meta_color.getAttribute('content') === "#323235"){meta_color.setAttribute('content', "hsl(0,0%,0%)");}
-  else{meta_color.setAttribute('content', "#323235");}}
-change_meta.addEventListener("click", change_color)
-
-
-
-
-	
+}).mount('#app')  	
 
 Splitting();
 	
@@ -305,6 +287,23 @@ function initBt2() {
     tl.restart();
   });
 }
+
+
+
+
+var change_meta = document.getElementById("wtr2");
+var meta_color = document.createElement("meta");
+    meta_color.setAttribute("name","theme-color");
+    meta_color.setAttribute("content","#323235");
+    document.getElementsByTagName("head")[0].appendChild(meta_color);
+function change_color(){  
+  if(meta_color.getAttribute('content') === "#323235"){meta_color.setAttribute('content', "hsl(0,0%,0%)");}
+  else{meta_color.setAttribute('content', "#323235");}}
+change_meta.addEventListener("click", change_color)
+
+
+
+	
 
 $('html, body').css({
   'overflow': 'auto',
