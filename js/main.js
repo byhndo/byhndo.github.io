5f565c1ce0c14c39ba9d5ba27e4a9f5a0333f85b
byhnd <120541041+byhnd@users.noreply.github.com>
2024-03-17 18:32:00 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -187,7 +187,7 @@ tl.to(one, {
  opacity:1,
  duration:.8,
  rotation:0,
- immediateRender:false,
+ immediateRender:true,
  y:0,
  stagger:.1,
  ease:easing	
@@ -214,7 +214,7 @@ tl.to(two, {
  opacity:1,
  y:0,
  duration:.8,
- immediateRender:false,
+ immediateRender:true,
  ease:easing
 });   
 
