1a3cf6e051635da32b175ea5ec8f5070e256e328
byhnd <120541041+byhnd@users.noreply.github.com>
2024-03-17 21:31:02 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -110,12 +110,10 @@ gsap.to(".ball", {
 
 gsap.set(".sub-title, .sub-title .char", {
  y:50,
- autoAlpha:0,
- opacity:0
+ autoAlpha:1,
+ opacity:1
 });
 gsap.to(".sub-title, .sub-title .char", {
- autoAlpha:1,
- opacity:0,
  delay:gsap.utils.wrap([.3, 1]),
  duration:"random(.5, 1)",
  stagger:.08, 
