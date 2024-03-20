98419118411b0605f7fd4282da01beac968f0754
byhnd <120541041+byhnd@users.noreply.github.com>
2024-03-20 12:35:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -73,6 +73,8 @@ gsap.to("nav", {
 	
 gsap.set("h1, h1 .char", { 
  y:100,
+ z:50,
+ rotationX:180,
  transformStyle: "preserve-3d",
  transformPerspective: 1000,
  opacity:0
@@ -81,8 +83,7 @@ gsap.to("h1, h1 .char", {
  opacity:1,
  duration:.8,
  stagger:.08,
- rotationX:180,
- z:50,
+ z:0,
  y:0,
  transformOrigin: "center center",
  ease:easing	
