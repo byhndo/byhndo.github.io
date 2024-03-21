bf777ad84a2f215f3a1254cb10614dec57a49768
byhnd <120541041+byhnd@users.noreply.github.com>
2024-03-21 10:14:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ tl.to(".textload span", {
 }); 
 		
 tl.to("#loader", {
- duration:.8,
+ duration:1,
  scaleX:.6,
  scaleY:.3,
  transformOrigin: "center"
@@ -26,7 +26,7 @@ tl.to("#loader", {
 
 tl.to("#loader", {
  delay:.2,
- duration:.8,
+ duration:1,
  scaleY:0,
  transformOrigin: "center"
 }); 
