9ad1a7ae14bf71874ae82780b5aa215216d0f07d
byhnd <120541041+byhnd@users.noreply.github.com>
2024-03-22 14:18:33 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -240,7 +240,7 @@ let revealContainers = container.querySelectorAll(".item");
   });
   tl.from(image, {
    duration:2.2,                   
-   scale: 1.4,   
+   scale:0,   
    ease:easing
   }, 0);
 	 	 
