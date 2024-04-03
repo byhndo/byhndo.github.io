8c775bfb6ca50b0bd4266f1ae1953e795dc7f806
byhnd <120541041+byhnd@users.noreply.github.com>
2024-04-03 16:40:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -256,10 +256,7 @@ const RevealBoxs3 = container.querySelectorAll(".box3");
  let tl = gsap.timeline({
  scrollTrigger: {
   trigger: box3,
-  toggleActions: once,
-  scrub: 4,
-  start: "top bottom",
-  end: "center center"
+  toggleActions: once
  }, delay: .5
  });  
 	 
@@ -279,14 +276,11 @@ tl.to(quote, {
 
 let revealContainers = container.querySelectorAll(".item");
  revealContainers.forEach((el) => {
- let image = el.querySelectorAll("img");
+ let image = el.querySelector("img");
  let tl = gsap.timeline({
  scrollTrigger: {
   trigger: el,
-  toggleActions: once,
-  scrub: 4,
-  start: "top bottom",
-  end: "center center"
+  toggleActions: once
   }, delay:.5
  });
 
