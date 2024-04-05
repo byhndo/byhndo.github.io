52e4ecfc9b6e21c0e854b459018fe01269a20c5e
byhnd <120541041+byhnd@users.noreply.github.com>
2024-04-05 09:53:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -326,10 +326,12 @@ let revealContainers = container.querySelectorAll(".item");
   }, delay:.5
  });
 
-tl.set(el, { autoAlpha: 1, scale:0, rotation:gsap.utils.wrap([-180, 180]) });
+tl.set(el, { autoAlpha: 1, scale:0,  x: () => gsap.utils.random(-200, 200), y: () => gsap.utils.random(-150, 150), rotation:gsap.utils.wrap([-180, 180]) });
 tl.to(el, {
  scale:1,
  duration: 1.5,
+ x: 0,
+ y: 0,
  rotation: 0,
  ease: "cubic.out"
 });  
