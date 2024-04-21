afc0c7a9d188447578302656f71bf151cfeec698
byhnd <120541041+byhnd@users.noreply.github.com>
2024-04-21 09:00:20 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,15 +12,10 @@ tl.to(".spintext", {
 tl.to(".spin", {
  opacity:0
 }, .7)
-
-tl.set(".textload span", {
- scale:0	
-});
 	
 tl.to(".textload span", {
  autoAlpha:1,
  opacity:1,
- scale:1,
  duration:1,
  stagger:.07,
  delay:"random(0, 1)",
