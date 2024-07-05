940ac48ea2602fd9e96c6f463b14064fae271a7b
byhnd <120541041+byhnd@users.noreply.github.com>
2024-07-05 14:51:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -297,6 +297,7 @@ const RevealBoxs3 = container.querySelectorAll(".box3");
  let tl = gsap.timeline({
  scrollTrigger: {
   trigger: box3,
+  scrub: 2,
   toggleActions: once
  }, delay: .3
  });  
