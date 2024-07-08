2c0198c7e0df1fa3a1376e7d9a0c975e6d822b2b
byhnd <120541041+byhnd@users.noreply.github.com>
2024-07-08 22:25:29 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,6 +48,45 @@ let tl = gsap.timeline({
     updateProgress(); */
 
 
+
+
+	
+
+function startLoader(){
+  let counter = document.querySelector('.counter');
+  let currentValue=0;
+  function updateCounter(){
+    if(currentValue===100){
+      return;
+    }
+    currentValue+=Math.floor(Math.random()*10)+1;
+    if(currentValue >100){
+      currentValue=100;
+    }
+    counter.textContent=currentValue
+    let delay=Math.floor(Math.random()*200)+50
+    setTimeout(updateCounter,delay);
+    
+  }
+  updateCounter();
+}
+
+tl.to('.counter',0.25,{
+  delay:3.5,
+  opactiy:0,
+  zIndex:-10000
+})
+tl.to('.bar',1.5,{
+  delay:3.5,
+  height:0,
+  stagger:{
+    amount:0.5,
+  },
+  ease:'power4.inOut'
+})
+	
+
+	
 	
 function contentShow() {
 
