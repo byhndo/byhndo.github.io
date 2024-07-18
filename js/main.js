c528845e8374976d171a2641aff1f62baa6329e3
byhnd <120541041+byhnd@users.noreply.github.com>
2024-07-18 16:52:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,7 +107,50 @@ createApp({
 
         afterLeave(el) {
             el.ctx && el.ctx.revert();
-        }
+        },
+
+
+
+beforeEnter(el) {
+      gsap.set(el, {
+        scaleX: 0.8,
+        scaleY: 1.2
+      })
+    },
+    enter(el, done) {
+      gsap.to(el, {
+        duration: 1,
+        scaleX: 1.5,
+        scaleY: 0.7,
+        opacity: 1,
+        x: 150,
+        ease: 'elastic.inOut(2.5, 1)',
+        onComplete: done
+      })
+    },
+    leave(el, done) {
+       gsap.to(el, {
+        duration: 0.7,
+        scaleX: 1,
+        scaleY: 1,
+        x: 300,
+        ease: 'elastic.inOut(2.5, 1)',
+      })
+      gsap.to(el, {
+        duration: 0.2,
+        delay: 0.5,
+        opacity: 0,
+        onComplete: done
+      })
+    },
+
+
+
+
+
+
+
+	    
     },	
 data() {
         return {
