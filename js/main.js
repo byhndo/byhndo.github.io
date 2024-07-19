9d4fb835b03290d696aa34ed4d9c749b5f1fe51c
byhnd <120541041+byhnd@users.noreply.github.com>
2024-07-19 17:02:03 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ let tl = gsap.timeline({
 function contentShow() {
 	
 const easing = "expo.out";
-const dur = 2;
+const dur = 1.5;
 const once = "play none none reset";
 	
 gsap.registerPlugin(ScrollTrigger);
