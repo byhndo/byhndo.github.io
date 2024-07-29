a8ff64d6cc9c620deec94e563d08163d4e9dd21b
byhnd <120541041+byhnd@users.noreply.github.com>
2024-07-29 18:25:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -221,16 +221,7 @@ function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    lenis.scrollTo(this.getAttribute('href'))
-  });
-})
-
 	
 	
 function setupReveal(container) {
