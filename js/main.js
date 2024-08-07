f4ba4b808f247192eacf4facd9bb4f271ddc75e1
byhnd <120541041+byhnd@users.noreply.github.com>
2024-08-07 10:19:02 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -214,8 +214,8 @@ const lenis = new Lenis({
   smooth: true,
   smoothTouch: true,
   smoothWheel: true,
-  touchMultiplier: 2,
-  wheelMultiplier: 1,
+  touchMultiplier: 3,
+  wheelMultiplier: 3,
   infinite: false,
   autoResize: true
 });
