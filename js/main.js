88583f9862479f011c15563aef1e1b9bc3ebb097
byhnd <120541041+byhnd@users.noreply.github.com>
2024-08-08 10:09:54 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ let tl = gsap.timeline({
     autoAlpha:0,
     width:0,
     duration:3,
-    ease: "elastic.out",
+    ease: "expo.inOut",
     onComplete: () => {
       loadingBar.style.display = 'none';
       tl.to(landing.intro, {
