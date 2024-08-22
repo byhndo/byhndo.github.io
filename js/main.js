587ff621a8b8e71f5f87fe4c96a628c739642f9f
byhnd <120541041+byhnd@users.noreply.github.com>
2024-08-22 14:38:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -417,6 +417,7 @@ const revealSc = container.querySelectorAll(".item-soc");
     
     tl.to(bttn, {
       autoAlpha: 0,
+      margin:0,
       onComplete: () => {
         bttn.style.opacity = "1";
         tl.to(bttn, {
