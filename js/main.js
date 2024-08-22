0461eb208e6dd5727ad3cda88be10f5cb20210f5
byhnd <120541041+byhnd@users.noreply.github.com>
2024-08-22 15:16:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -422,8 +422,7 @@ const revealSc = container.querySelectorAll(".item-soc");
         tl.to(bttn, {
           duration: 1,
           onComplete: () => {
-            bttn.style.visibility = "visible";	
-	    bttn.style.margin = "0";
+            bttn.style.visibility = "visible";		    
             bttn.addEventListener("click", () => {
               particles.disintegrate();
             });
