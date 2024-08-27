89083b425b1b53eb476cf6362230e8a84a5defbb
byhnd <120541041+byhnd@users.noreply.github.com>
2024-08-27 11:10:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,9 +147,9 @@ tl.to(primitiveValues, {
     startAt: { stdDeviation: 30, scale: 150 },  
     stdDeviation: 0,  
     scale: 0 
-  }, 0)
+  }, 0);
 
-.to("h1", {
+tl.to("h1", {
     startAt: {
       opacity: 0,  
       scale: 0.9      
@@ -351,9 +351,9 @@ tl.to(primitiveValues, {
     startAt: { stdDeviation: 35, scale: 250 },  
     stdDeviation: 0,  
     scale: 0
-  }, 0)
+  }, 0);
 
-.to(one, {
+tl.to(one, {
   duration: 1.3,
     startAt: {
       opacity: 0,  
