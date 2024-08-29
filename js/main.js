3c6c5885177bb74564bbee5bc35c4526e7d7b5e4
byhnd <120541041+byhnd@users.noreply.github.com>
2024-08-29 16:55:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -222,8 +222,7 @@ tl.to(items, {
        opacity:1
         });
 
-window.addEventListener("mouseout", ()=> {
-  
+window.addEventListener("mouseover", ()=> {  
   tl.to(bttn, {
       onComplete: () => {
         particles.integrate({
