231fbe601cf1bc5e1924188c97f941f99c2b9346
byhnd <120541041+byhnd@users.noreply.github.com>
2024-09-12 13:30:26 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ gsap.to(bttn, {
       autoAlpha: 0,
       onComplete: () => {
         particles.integrate({
-          duration: 910,
+          duration: 900,
           easing: "easeOutSine"
         });
         gsap.to(bttn, {
