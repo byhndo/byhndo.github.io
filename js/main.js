d6ef802ce480501126ac97bb6776d0b2b0b9802c
byhnd <120541041+byhnd@users.noreply.github.com>
2024-09-12 14:16:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -93,11 +93,11 @@ gsap.to(bttn, {
       autoAlpha: 0,
       onComplete: () => {
 	particles.integrate({
-          duration: 1000,
+          duration: 890,
           easing: "easeOutSine"
         });
        gsap.to(bttn, {
-         duration: 1.1,
+         duration: 1,
 	 ease: "sine.out",
           onComplete: () => {
             bttn.style.opacity = "1";
