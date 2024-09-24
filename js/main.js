574fd7636565689f31bfd88546d4260528ce904f
byhndo <120541041+byhndo@users.noreply.github.com>
2024-09-24 15:46:03 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -367,12 +367,17 @@ const lenis = new Lenis({
  autoResize: true
 });
 
-lenis.on("scroll", ({ scroll, limit, velocity, direction, progress}) => {});
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
 	
 function setupReveal(container) {
 	
