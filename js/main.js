901812fad5002ccc8de2240f10e82c57042a299d
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-02 14:47:22 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,17 +41,17 @@ async function animateLoader() {
   DOM.intro = document.querySelector(".preloader-wrap");
   DOM.shape = DOM.intro.querySelector("svg.shape");
   DOM.path = DOM.intro.querySelector("path.goey");
-	
-  let tl = gsap.timeline({
-    paused: true,
-    onComplete: contentShow
-  });
 
   gsap.to(".percentage:before, .percentage:after", {
     duration:.5,
     ease: "elastic.out"
   })
 	
+  let tl = gsap.timeline({
+    paused: true,
+    onComplete: contentShow
+  });
+  	
   tl.to(".percentage", {
     autoAlpha: 0,
     duration: 1,
