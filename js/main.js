a441f46da835a5abc874adf1cf9386f81b84fc08
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-11 14:01:04 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ async function animateLoader() {
     onComplete: contentShow
   });
 
-  tl.to(percentBar, loadingBar, {
+  tl.to("#percent", "#loader", {
    delay:3,
    duration: 1,
    autoAlpha:1
