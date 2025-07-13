bd5200784d4a945730d0435a13bfe664980f923f
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-13 11:51:25 +0700

    Update morp.js

--- morp.js
+++ morp.js
@@ -15,10 +15,9 @@ const paths = {
     },
 
     step2: {
-      filled: "M 100 0 H 0 c 0 50 0 50 0 100 h 100 V 50 Z",
-      //inBetween: 'M 100 0 H 50 c 20 33 20 67 0 100 h 50 V 0 Z',
-      inBetween: "M 100 0 H 50 c 28 43 4 81 0 100 h 50 V 0 Z",
-      unfilled: "M 100 0 H 100 c 0 50 0 50 0 100 h 0 V 0 Z"
+      unfilled: "M 100 0 h 0 c 0 50 0 50 0 100 H 100 V 0 Z",  
+      inBetween: "M 100 0 h -33 c 30 54 -113 65 0 100 H 100 V 0 Z",
+      filled: "M 100 0 h -100 c 0 50 0 50 0 100 H 100 V 0 Z"
     }
   };
 
@@ -45,19 +44,19 @@ const tl = gsap.timeline({
   const tl2 = gsap.timeline({paused: true}) 
 
     .set(photosPath, {
-      attr: { d: paths.step1.unfilled }
+      attr: { d: paths.step2.unfilled }
     })
 
     .to(photosPath, {
         duration: 0.8,
         ease: "power3.in",
-        attr: { d: paths.step1.inBetween }
+        attr: { d: paths.step2.inBetween }
       }, 0)
 
     .to(photosPath, {
       duration: 0.2,
       ease: "power1",
-      attr: { d: paths.step1.filled }
+      attr: { d: paths.step2.filled }
     });
 
 const tl3 = gsap.timeline({ paused: true })
