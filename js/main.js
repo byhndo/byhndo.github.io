bd17e1e269124a7d73b0bbd5f2be3a40cf05ff6b
byhndo <120541041+byhndo@users.noreply.github.com>
2025-07-17 15:02:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -216,7 +216,7 @@ const titletl = gsap.timeline({
   })
 
   .to(primitiveValues, {
-      startAt: { stdDeviation: 1, scale: 150 },
+      startAt: { stdDeviation: 1, scale: 300 },
       stdDeviation: 0,
       scale: 0}, 0)
   .to(title, {
